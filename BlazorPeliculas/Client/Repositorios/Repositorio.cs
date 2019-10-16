c661ec2baf1f4cd47511e06b9d72e38c46afe2b9
Jordi Vilaró <jvilarof@summar.es>
2019-10-16 08:15:03 +0200

    Implementar la inyencción de dependencias

---
+++ BlazorPeliculas/Client/Repositorios/Repositorio.cs
@@ -0,0 +1,19 @@
+﻿using BlazorPeliculas.Shared.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorPeliculas.Client.Repositorios
+{
+  public class Repositorio : IRepositorio
+  {
+    public List<Pelicula> ObtenerPeliculas()
+    {
+      return new List<Pelicula>
+      {
+        new Pelicula() { Titulo = "Spiderman - Lejos de casa", Lanzamiento = new DateTime(2019, 6, 20) },
+        new Pelicula() { Titulo = "Vaiana", Lanzamiento = new DateTime(2017, 8, 12) },
+        new Pelicula() { Titulo = "Origen", Lanzamiento = new DateTime(2010, 5, 15) }
+      };
+    }
+  }
+}
