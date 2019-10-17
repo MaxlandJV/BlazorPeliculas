dd1a4bfaa323a5d0baf32a8ee1fe3927874b4e69
Jordi Vilaró <jvilarof@summar.es>
2019-10-17 07:59:36 +0200

    Herencia y Layouts

---
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorPeliculas.Client.Pages
+{
+  public class CounterBase : ComponentBase
+  {
+    [Inject] protected ServicioSingleton Singleton { get; set; }
+    [Inject] protected ServicioTransient Transient { get; set; }
+
+    protected int currentCount = 0;
+
+    protected void IncrementCount()
+    {
+      currentCount++;
+      Singleton.Valor = currentCount;
+      Transient.Valor = currentCount;
+    }
+  }
+}
