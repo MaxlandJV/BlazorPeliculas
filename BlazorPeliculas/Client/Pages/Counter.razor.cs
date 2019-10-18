e10ffb112be2c95744e0efe3a90526dcea0f30ac
MaxlandJV <jvilarof@gmail.com>
2019-10-18 11:53:04 +0200

    Invocar C# desde JavaScript

--- BlazorPeliculas/Client/Pages/Counter.razor.cs
+++ BlazorPeliculas/Client/Pages/Counter.razor.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorPeliculas.Client.Pages
 {
@@ -6,14 +8,24 @@ namespace BlazorPeliculas.Client.Pages
   {
     [Inject] protected ServicioSingleton Singleton { get; set; }
     [Inject] protected ServicioTransient Transient { get; set; }
+    [Inject] protected IJSRuntime JS { get; set; }
 
     protected int currentCount = 0;
+    static int currentCountStatic = 0;
 
-    protected void IncrementCount()
+    protected async Task IncrementCount()
     {
       currentCount++;
       Singleton.Valor = currentCount;
       Transient.Valor = currentCount;
+      currentCountStatic++;
+      await JS.InvokeVoidAsync("pruebaPuntoNetStatic");
+    }
+
+    [JSInvokable]
+    public static Task<int> ObtenerCurrentCount()
+    {
+      return Task.FromResult(currentCountStatic);
     }
   }
 }
