26511b1aebab7256151af50de900aa00c8c782b9
Jordi Vilaró <jvilarof@summar.es>
2019-10-30 07:54:01 +0100

    Modelo de persona y formularios para crear y editar personas

---
+++ BlazorPeliculas/Shared/Entidades/Persona.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+  public class Persona
+  {
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    public string Nombre { get; set; }
+    public string Biografia { get; set; }
+    public string Foto { get; set; }
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    public DateTime? FechaNacimiento { get; set; }
+  }
+}
