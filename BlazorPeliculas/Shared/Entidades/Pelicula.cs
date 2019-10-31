350c7ee1131e4fbba22f86e1a2d94be6cf482ce6
Jordi Vilaró <jvilarof@summar.es>
2019-10-31 08:32:15 +0100

    Modelo y componentes para creación  y edición de películas con selección múltiple de géneros

--- BlazorPeliculas/Shared/Entidades/Pelicula.cs
+++ BlazorPeliculas/Shared/Entidades/Pelicula.cs
@@ -1,12 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorPeliculas.Shared.Entidades
 {
   public class Pelicula
   {
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
     public string Titulo { get; set; }
-    public DateTime Lanzamiento { get; set; }
+    public string Resumen { get; set; }
+    public bool EnCartelera { get; set; }
+    public string Trailer { get; set; }
+    [Required(ErrorMessage = "El campo {0} es obligatorio")]
+    public DateTime? Lanzamiento { get; set; }
     public string Poster { get; set; }
+    public List<GeneroPelicula> GeneroPelicula { get; set; } = new List<GeneroPelicula>();
     public string TituloCortado
     {
       get
